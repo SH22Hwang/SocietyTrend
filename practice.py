17caf96d8f8fdfd94c97a871e33bdbe9529e9076
SH22Hwang <70620484+SH22Hwang@users.noreply.github.com>
2021-11-29 22:08:53 +0900

    selenium 드롭다운 메뉴 크롤링에서 막힘..?

--- practice.py
+++ practice.py
@@ -6,22 +6,33 @@ from selenium.webdriver.support.ui import Select
 url = 'http://scholar.dkyobobook.co.kr/searchExtAcademy.laf?vendorGb=05&academyCd=20580'
 
 driver = webdriver.Chrome('chromedriver')
-# driver.implicitly_wait(3)
-# driver.get(url)
+driver.get(url)
+
+select = Select(driver.find_element_by_name('volSel'))
 
 trendList = []
 
-response = requests.get(url)
+response = select.select_by_value('144521')
+# response = requests.get(url)
 
-if response.status_code == 200:
-    html = response.text
-    soup = BeautifulSoup(html, 'html.parser')
-    div = soup.select_one('div.searchExtAcademy')
-    titles = div.select('div.paper_info > div.paper_box > dl > dt > a')
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+div = soup.select_one('div.searchExtAcademy')
+titles = div.select('div.paper_info > div.paper_box > dl > dt > a')
 
-    for title in titles:
-        trendList.append(title.get_text())
+for title in titles:
+    trendList.append(title.get_text())
+print(trendList)
 
-    print(trendList)
-else:
-    print(response.status_code)
+# if response.status_code == 200:
+#     html = response.text
+#     soup = BeautifulSoup(html, 'html.parser')
+#     div = soup.select_one('div.searchExtAcademy')
+#     titles = div.select('div.paper_info > div.paper_box > dl > dt > a')
+#
+#     for title in titles:
+#         trendList.append(title.get_text())
+#
+#     print(trendList)
+# else:
+#     print(response.status_code)
